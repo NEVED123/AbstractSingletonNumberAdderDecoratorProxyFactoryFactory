e50b2aa783feb86c7e2fa34c9186f1a70b1f6b0a
Deven Mallamo <neved1234@gmail.com>
2024-09-01 21:05:20 -0400

    Immense productivity gains in the future

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
+import java.util.concurrent.TimeUnit;
+import java.lang.InterruptedException;
+
 class Main {
     public static void main(String[] args) {
         NumberAdderFactory numberAdderFactory = NumberAdderFactoryFactory.CreateNumberAdderFactoryInstance(1, 2);
         NumberAdder numberAdder = numberAdderFactory.CreateNumberAdderInstance("Not Loaded!");
-        numberAdder.load();
+        System.out.println("Loading number adder... the extreme cleverness of this code is unparalleled!");
+        try {
+            numberAdder.load();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println(numberAdder.add());
     }
 }
@@ -15,8 +23,7 @@ class BoringOldAdder {
 
 //Abstract - Defines the interface for the object without defining the implementation
 abstract class NumberAdder {
-
-    public abstract void load();
+    public abstract void load() throws InterruptedException;
     public abstract int add();
 }
 
@@ -34,7 +41,7 @@ class NumberAdderFactory {
 
     //Factory - handles initialization of objects
     public NumberAdder CreateNumberAdderInstance(String errorMessage) {
-        return new ConcreteAdder(a, b, boringOldAdder, errorMessage);
+        return ConcreteAdder.getNumberAdderInstance(a, b, boringOldAdder, errorMessage);
     }
 
 }
@@ -53,9 +60,19 @@ class ConcreteAdder extends NumberAdder{
     private String errorMessage;
 
     //Decorator - Adds functionality to an object by wrapping it in another object.
-    private BoringOldAdder boringOldAdder = new BoringOldAdder();
+    private BoringOldAdder boringOldAdder;
+    private static ConcreteAdder instance;
 
-    public ConcreteAdder(int a, int b, BoringOldAdder boringOldAdder, String errorMessage) {
+    //Singleton - Only one instance of the object can be created
+    public static NumberAdder getNumberAdderInstance(int a, int b, BoringOldAdder boringOldAdder, String errorMessage) {
+        if (instance == null) {
+            instance = new ConcreteAdder(a, b, boringOldAdder, errorMessage);
+        }
+
+        return instance;
+    }
+
+    private ConcreteAdder(int a, int b, BoringOldAdder boringOldAdder, String errorMessage) {
         this.a = a;
         this.b = b;
         this.boringOldAdder = boringOldAdder;
@@ -65,7 +82,8 @@ class ConcreteAdder extends NumberAdder{
 
     //Proxy: A representation of a class that takes its "full form" when required at runtime, usually to avoid the overhead of creating a lot of a large objects at once.
     @Override
-    public void load() {
+    public void load() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(5); // We can get rid of this before the next retrospective so the manager sees improvement in code performance 
         loaded = true;
     }
 
